2abbd210b131617390adf6b8dc9a1af957ad31dc
domini251 <kdm030901@gmail.com>
2023-12-02 18:35:14 +0900

    TK-33 hotfix: fixed unappropriate ui position & sound status melfunction in 2p mode.

--- src/screen/GameScreen_2P.java
+++ src/screen/GameScreen_2P.java
@@ -447,11 +447,12 @@ public class GameScreen_2P extends Screen {
             cleanBulletsY_1P();
             cleanBulletsY_2P();
             cleanItems();
-            draw();
         }
+        draw();
         if (this.enemyShipFormation.isEmpty() && !this.levelFinished) {
             endStageAllEat();
             bgm.enemyShipSpecialbgm_stop();
+            bgm.InGame_bgm_stop();
             this.levelFinished = true;
             this.screenFinishedCooldown.reset();
             timer.stop();
@@ -644,7 +645,7 @@ public class GameScreen_2P extends Screen {
                 drawManager.drawBossLivesbar(this, enemyShip.getEnemyLife());
         }
         drawManager.drawHorizontalLine(this, SEPARATION_LINE_HEIGHT - 1);
-        if (inputManager.isKeyDown(KeyEvent.VK_C)) {
+        if (inputManager.isKeyPressedOnce(KeyEvent.VK_C)) {
             isSoundOn = !isSoundOn;
             if (isSoundOn) {
                 bgm.InGame_bgm_play();
@@ -662,7 +663,6 @@ public class GameScreen_2P extends Screen {
         drawManager.changeGhostColor_2p(this.levelFinished, this.lives_1p, this.lives_2p);
         drawManager.drawGhost_2p(this.levelFinished, this.lives_1p, this.lives_2p);
         this.ship_1P.gameEndShipMotion(this.levelFinished, this.lives_1p);
-
         this.ship_2P.gameEndShipMotion(this.levelFinished, this.lives_2p);
 
 
@@ -687,7 +687,7 @@ public class GameScreen_2P extends Screen {
         if (this.pause) {
             drawManager.drawPaused(this);
             drawManager.drawSoundButton2(this);
-            if (inputManager.isKeyDown(KeyEvent.VK_C)) {
+            if (inputManager.isKeyPressedOnce(KeyEvent.VK_C)) {
                 isSoundOn = !isSoundOn;
                 if (isSoundOn) {
                     bgm.InGame_bgm_play();
