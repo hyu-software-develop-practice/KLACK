5d24b88f57e62af6ca0dbc047effcd7acd58b3f5
domini251 <kdm030901@gmail.com>
2023-12-10 18:18:54 +0900

    testing release automation

--- .github/versioning.sh
+++ .github/versioning.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-VERSION=""
+Vesion=""
 
 #get parameters
-VERSION=$LABEL
+Version=$LABEL
 
 #get highest tag number, and add 1.0.0 if doesn't exist
 CURRENT_VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
 
 if [[ $CURRENT_VERSION == '' ]]
 then
-  CURRENT_VERSION='v0.1.0'
+  CURRENT_VERSION=$VERSION
 fi
 echo "Current Version: $CURRENT_VERSION"
 
@@ -23,13 +23,13 @@ VNUM1=${CURRENT_VERSION_PARTS[0]}
 VNUM2=${CURRENT_VERSION_PARTS[1]}
 VNUM3=${CURRENT_VERSION_PARTS[2]}
 
-if [[ $VERSION == 'major' ]]
+if [[ $Version == 'major' ]]
 then
   VNUM1=$((VNUM1+1))
-elif [[ $VERSION == 'minor' ]]
+elif [[ $Version == 'minor' ]]
 then
   VNUM2=$((VNUM2+1))
-elif [[ $VERSION == 'patch' ]]
+elif [[ $Version == 'patch' ]]
 then
   VNUM3=$((VNUM3+1))
 else
@@ -40,7 +40,7 @@ fi
 
 #create new tag
 NEW_TAG="v$VNUM1.$VNUM2.$VNUM3"
-echo "($VERSION) updating $CURRENT_VERSION to $NEW_TAG"
+echo "($Version) updating $CURRENT_VERSION to $NEW_TAG"
 
 #get current hash and see if it already has a tag
 GIT_COMMIT=`git rev-parse HEAD`
